c70298b50aa55599d7c9746cbf965d17e03b62c7
meemanshoo <boss.meemanshoo.cool@gmail.com>
2023-05-10 17:09:44 +0530

    firebase resetting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = require('./app')
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2323;
 
 const server = http.createServer(app);
 
