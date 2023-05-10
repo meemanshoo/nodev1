7deeb2bdc092fff6d5da5c317476dbd6329f0c4c
meemanshoo <boss.meemanshoo.cool@gmail.com>
2023-05-10 15:25:32 +0530

    First commit

---
+++ api/routes/student.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const router = express.Router();
+const Student = require('../model/student');
+const mongoose = require('mongoose');
+
+router.get('/',(req,res,next) => {
+    Student.find().then(result => {
+        res.status(200).json({
+            studentData:result
+        });
+    }).catch(err=>{
+        res.status(500).json({
+            error:err
+        });
+    });
+    // res.status(200).json({
+    //     msg:"this is student get request"
+    // });
+});
+
+router.get('/:id',(req,res,next) => {
+    Student.findById(req.params.id).then(result => {
+        res.status(200).json({
+            studentData:result
+        });
+    }).catch(err=>{
+        res.status(500).json({
+            error:err
+        });
+    });
+});
+
+router.post('/',(req,res,next) => {
+
+    const student = new Student({
+        _id:new mongoose.Types.ObjectId,
+        name:req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        gender:req.body.gender
+    });
+    
+    student.save().then(
+        result =>{
+        res.status(200).json({
+            newStudent:result
+        })
+        }
+    ).catch(err => {
+        res.status(500).json({
+            error:err
+        })
+    });
+});
+
+router.delete('/:id',(req,res,next) => {
+    Student.findByIdAndRemove({_id:req.params.id}).then(
+        result =>{
+        res.status(200).json({
+            message:'data deleted'
+        })
+        }
+    ).catch(err => {
+        res.status(500).json({
+            error:err
+        })
+    });
+});
+
+router.put('/:id',(req,res,next) => {
+    Student.findOneAndUpdate({_id:req.params.id},{
+        $set:{
+            name:req.body.name,
+            email:req.body.email,
+            phone:req.body.phone,
+            gender:req.body.gender
+        }
+    }).then(
+        result =>{
+        res.status(200).json({
+            message:'data Updated'
+        })
+        }
+    ).catch(err => {
+        res.status(500).json({
+            error:err
+        })
+    });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const app = express();
+
+//mongo pass - zysPz6ifrWXETmPV
+
+//aws pass -"26Tr*VZQFAhTS
+
+const studentRoute = require('./api/routes/student');
+const facultyRoute = require('./api/routes/faculty');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb+srv://Meemanshoo:zysPz6ifrWXETmPV@cluster0.x44mmbb.mongodb.net/?retryWrites=true&w=majority');
+
+mongoose.connection.on('error',err => {
+    console.log('connection failed');
+});
+
+mongoose.connection.on('connected',connected => {
+    console.log('connection successfull');
+});
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use('/student',studentRoute);
+app.use('/faculty',facultyRoute);
+
+app.use((req,res,next) => {
+    res.status(404).json({
+        error:"bad request"
+    });
+});
+
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message:'Its works!'
+//     });
+// });
+
+module.exports = app;
